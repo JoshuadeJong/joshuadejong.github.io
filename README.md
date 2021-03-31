8bf8ca0797b5a5d6f9c3fdcca5b0bd210e7d56ff
Joshua de Jong <joshuakdejong@gmail.com>
2021-03-30 21:50:21 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# [ManVanMaan.github.io](manvanmaan.github.io)
+
+This repository contains and hosts my portfolio as a static website. The flask source code used to build and deploy this website can be found in my other repository [github.com/ManVanMaan/portfolio](https://github.com/ManVanMaan/portfolio).
---
+++ static/js/terminal.js
@@ -0,0 +1,21 @@
+$(function() {
+  $(".typed").typed({
+    strings: [
+      "./josh.sh info<br/><span class='caret'>$</span>skills: coding, data analytics, machine learning, optimization<br/> ^100<span class='caret'>$</span>job: Software Engineer at <a href='http://www.expeditors.com//'>Expeditors CHQ</a><br/> ^100<span class='caret'>$</span>alias: MaanMan<br/> ^100<span class='caret'>$</span>hobbies: cooking, cello, cycling, video games, and beer tasting<br/> ^100<span class='caret'>$</span>highlight:  <a href='https://github.com/JoshuadeJong/portfolio'>Portfolio</a>, <a href='https://github.com/JoshuadeJong/Notakto'>Notakto</a><br/> ^100"
+    ],
+    showCursor: true,
+    cursorChar: '_',
+    autoInsertCss: true,
+    typeSpeed: 0.0001,
+    startDelay: 50,
+    loop: false,
+    showCursor: false,
+    onStart: $('.message form').hide(),
+    onStop: $('.message form').show(),
+    onTypingResumed: $('.message form').hide(),
+    onTypingPaused: $('.message form').show(),
+    onComplete: $('.message form').show(),
+    onStringTyped: function(pos, self) {$('.message form').show();},
+  });
+  $('.message form').hide()
+});
